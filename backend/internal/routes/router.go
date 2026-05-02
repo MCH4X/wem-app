b6e0b093405d1d6d1a4a646fbcfc7de292cd36c6
thanaporn-dev <tanaporn.dev@gmail.com>
2026-05-02 23:57:58 +0700

    init: project structured

--- backend/internal/routes/router.go
+++ backend/internal/routes/router.go
@@ -1,10 +1,22 @@
 package routes
 
 import (
-	"github.com/MCH4X/wem-app/backend/internal/handlers"
+	handler "github.com/MCH4X/wem-app/backend/internal/handlers"
+	repository "github.com/MCH4X/wem-app/backend/internal/repositories"
+	service "github.com/MCH4X/wem-app/backend/internal/services"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func SetupRouter(router *gin.Engine) {
-	router.GET("/", handlers.HelloHandler)
+func SetupRouter(db *gorm.DB) {
+	router := gin.Default()
+
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	router.GET("/users", userHandler.GetUsers)
+	router.POST("/users", userHandler.CreateUser)
+
+	router.Run(":8080")
 }
